6a07b631841b55fdce92cbebf3de07c84673cf48
Skenvy <17214791+Skenvy@users.noreply.github.com>
2025-02-05 19:13:59 +1100

    ye

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,116 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import { graphql } from '@octokit/graphql'
 
-try {
-  // `name-of-input` input defined in action metadata file
-  const nameToGreet = core.getInput('name-of-input')
-  console.log(`Hello ${nameToGreet}!`)
-  const time = new Date().toTimeString()
-  core.setOutput('name-of-output', time)
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`)
-} catch (error) {
-  if (error instanceof Error) core.setFailed(error.message)
+async function run() {
+  try {
+    const context = github.context
+    const eventName = context.eventName
+
+    // If this isn't running under a PR trigger, annotate and leave early
+    if (eventName !== 'pull_request') {
+      core.setFailed(
+        `dispatch-suggestor can only be run from a pull_request event, but it was triggered by a ${eventName} event.`
+      )
+      return
+    }
+
+    // Otherwise, procede as usual
+    const token = core.getInput('github_token')
+
+    // EXAMPLE LEFTOVER
+    // const nameToGreet = core.getInput('name-of-input')
+    // console.log(`Hello ${nameToGreet}!`)
+    // const time = new Date().toTimeString()
+    // core.setOutput('name-of-output', time)
+    // // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`)
+
+    async function getPRNumber() {
+      return context.payload.pull_request
+        ? context.payload.pull_request.number
+        : null
+    }
+
+    const owner = context.repo.owner
+    const repo = context.repo.repo
+    const pullRequestNumber = await getPRNumber()
+
+    const gql_query_list_PR_files = `
+      query($owner: String!, $name: String!, $pullRequestNumber: Int!) {
+        repository(owner: $owner, name: $name) {
+          pullRequest(number: $pullRequestNumber) {
+            files(first: 1000) {
+              edges {
+                node {
+                  path
+                  additions
+                  deletions
+                  changeType
+                }
+              }
+            }
+          }
+        }
+        rateLimit {
+          cost
+          remaining
+          resetAt
+        }
+      }
+    `
+    interface GQLQueryListPRFiles {
+      data: {
+        repository: {
+          pullRequest: {
+            files: {
+              edges: {
+                node: {
+                  path: string
+                  additions: number
+                  deletions: number
+                  changeType: string
+                }
+              }[]
+            }
+          }
+        }
+        rateLimit: {
+          cost: number
+          remaining: number
+          resetAt: string
+        }
+      }
+    }
+
+    async function fetchChangedFiles() {
+      try {
+        const result = await graphql<GQLQueryListPRFiles>({
+          gql_query_list_PR_files,
+          owner,
+          name: repo,
+          pullRequestNumber,
+          headers: {
+            authorization: `token ${token}`
+          }
+        })
+
+        const files = result.data.repository.pullRequest.files.edges.map(
+          (edge) => edge.node
+        )
+        const rateLimitInfo = result.data.rateLimit
+        console.log('Changed files:', files)
+        console.log('Rate Limit Info:', rateLimitInfo)
+      } catch (error) {
+        console.error('Error fetching changed files:', error)
+      }
+    }
+
+    fetchChangedFiles()
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
 }
+
+await run()
