ad8a2c57bdc1ab73c6763904dd9d47675a6e4e1b
Skenvy <17214791+Skenvy@users.noreply.github.com>
2025-02-16 19:56:36 +1100

    rollup

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,16 @@
-export {};
+/**
+ * A 1:1 of the inputs expected by the action.yml
+ */
+export type ActionInputs = {
+    github_token: string;
+    trunk_branch: string;
+    checkout_root: string;
+    log_event_payload: string;
+    log_workflow_triggers: string;
+};
+/**
+ * Manages getting all the parameter inputs prior to running the action.
+ * @returns Promise<ActionInputs | null>
+ */
+export declare function getActionInputs(): Promise<ActionInputs | null>;
+export declare function run(actionInputs: ActionInputs): Promise<void>;
